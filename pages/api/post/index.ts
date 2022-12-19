e6721e7c684236181dac439c7e891d02764372f1
LarryAustinIyere <austinlarry1506@gmail.com>
2022-12-20 00:15:31 +0100

    added zustand for state context

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -3,10 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { client } from '../../../utils/client';
 import { allPostsQuery } from '../../../utils/queries';
 
-type Data = {
-    name: string
-}
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         const query = allPostsQuery();
@@ -14,5 +10,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const data = await client.fetch(query);
 
         res.status(200).json(data);
+    } else if (req.method === 'POST') {
+        const document = req.body;
+
+        client.create(document)
+            .then(() => res.status(201).json('Video Created'))
     }
 }
